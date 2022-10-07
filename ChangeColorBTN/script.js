8086f123b422cd7351485bc9ffaed891009d5be4
rzasouza <rychardzousa@gmail.com>
2022-10-07 13:09:56 -0300

    onClickBtn

---
+++ ChangeColorBTN/script.js
@@ -0,0 +1,6 @@
+const changeBTN = document.getElementById('btn');
+
+changeBTN.onclick = function newColor() {
+    changeBTN.style.backgroundColor = 'red';
+    changeBTN.style.color = 'white';
+};
